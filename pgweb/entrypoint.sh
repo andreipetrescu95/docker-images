88c23da81bf1dd71a67b07f5c26aa05e001f5579
Andrei Miulescu <lusu777@gmail.com>
2019-07-23 10:03:36 +0300

    Update entrypoint

--- pgweb/entrypoint.sh
+++ pgweb/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
-/usr/bin/pgweb --sessions --bind=0.0.0.0 --listen=8081 --url=${DATABSE_URL}
+/usr/bin/pgweb --bind=0.0.0.0 --listen=8081 --host=${DB_HOST} --port=${DB_PORT} \
+      --user=${DB_USER} \
+      --pass=${DB_PASSWORD} \
+      --db=${DB_NAME} \
+      --ssl=disable
